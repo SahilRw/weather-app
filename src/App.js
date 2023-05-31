b9c19fb5d4eea1aee4108b13fcb2bad2f138d3fe
Sahil R <sahilrw000@gmail.com>
2023-05-31 13:48:06 +0530

    background updates, api key changes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState('')
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=abdaa8f98ac6f6dc518588c84cda82c0`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=6d4a2aad761dbafa3e3a939bf081c171`;
   const searchLocation = (event) => {
     if (event.key === 'Enter') {
       axios.get(url).then((response) => {
         setData(response.data)
+        console.log(response.data)
       })
       setLocation('')
     }
